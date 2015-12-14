39c92b1296517cfae132a3c669dc8fbb84a0ef04
PREET GUJRAL <PREET@PREETGUJRAL.COM>
2015-12-13 17:02:04 -0800

    get project up to date with GH

--- public/controllers/controller.js
+++ public/controllers/controller.js
@@ -5,7 +5,8 @@ Todo.controller('TaskCtrl', ['$scope', TaskCtrl]);
 function TaskCtrl($scope) {
     $scope.tasks = [
         {
-            description: 'some task description - task 1'
+            description: 'some task description - task 1',
+            done: false
         },
         {
             description: 'some task description - task 2'
